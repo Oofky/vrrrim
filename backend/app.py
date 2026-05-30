8712250cb07221288b66578455bac38496fc7142
Oofky <beensprouting@gmail.com>
2026-05-30 22:17:24 +0800

    Add basic room code functionality
    Room code logic heavily inspired by skribbl.io's and TypeRacer's, where:
    
    - the index page and race page both use the same endpoint, and
    - joining private rooms involve using the first URL parameter given

--- backend/app.py
+++ backend/app.py
@@ -1,25 +1,51 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
+import random, string
 
 app = Flask(__name__, template_folder='../frontend', static_folder='../frontend', static_url_path='/')
+app.secret_key = 'temporary' #TODO: Change to be secure
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'GET':
+        if len(request.args) > 0: # Joining private room / Invalid room code
+            room_code = next(iter(request.args)) # Get first parameter
+            if room_exists(room_code): 
+                session['code'] = room_code
+                return render_template('index.html')
+            
+        # Else if no arg or invalid room code, access index page without setting room code
+        session.pop('code', None)
+        return render_template('index.html')
 
-@app.route('/play')
-def play():
-    # When users are redirected here, they either have a URL parameter of the room code or not.
-    # If no URL parameter: implies they are the room host (they created the room).
-    # If URL parameter: implies they were sent the URL by the room host / not real room code.
-    
-    if len(request.args) == 0:
-        #TODO: generate new room code, store it.
-        return render_template('race.html')
-    else:
-        room_code = next(iter(request.args)) # First parameter
-        print(room_code)
-        #TODO: check if room_code exists. if yes, join the private room. else, join a public room.
-        return render_template('race.html')
+    else: # Player clicked 'Play' or 'Create private room' or invalid POST request
+        clicked = request.form.get('clicked')
+
+        if clicked == 'play':
+            room_code = session.get('code')
+            if not room_code: # Join a public room
+                room_code = find_room()
+                session['code'] = room_code
+            return render_template('race.html', code=session['code'])
+
+        elif clicked == 'private':
+            session['code'] = generate_private_room()
+            return render_template('race.html', code=session['code'])
+                
+        # Else if invalid POST request, return index page
+        session.pop('code', None)
+        return render_template('index.html')
+
+def room_exists(code):
+    #TODO: Check if room code exists in database AND is open. Retuns True if it exists and is open.
+    return True
+
+def find_room():
+    #TODO: Find a public AND open room code in database. If none available, generate new code and add to db as public room.
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=8))
+
+def generate_private_room():
+    #TODO: Generate new code and add to database as private room.
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=8))
 
 if __name__ == '__main__':
     app.run(host='localhost', port=5000, debug=True)
